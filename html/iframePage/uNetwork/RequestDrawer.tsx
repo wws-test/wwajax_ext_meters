f95c1982cdbccc4d2eef4c85053f9192cbc4552c
Luciana Denley <1092587222@qq.com>
2023-12-06 17:32:27 +0800

    加入查询接口

--- html/iframePage/uNetwork/RequestDrawer.tsx
+++ html/iframePage/uNetwork/RequestDrawer.tsx
@@ -2,7 +2,31 @@ import { Divider, Drawer, Tabs } from 'antd';
 import React, { useEffect, useState } from 'react';
 import { FilterOutlined } from '@ant-design/icons';
 import './RequestDrawer.css';
+import * as CryptoJS from 'crypto-js';
+import { v4 as uuidv4 } from 'uuid';
 
+const aesEncrypt = (text: string, secretKey: string, iv: string): string => {
+  // @ts-ignore
+  const encrypted = CryptoJS.AES.encrypt(text, secretKey, { iv: iv });
+  return encrypted.toString();
+};
+
+const setHeaders = (s: any, accessKey: string, secretKey: string): any => {
+  const timeStamp = new Date().getTime();
+  const combox_key = accessKey + '|' + uuidv4() + '|' + timeStamp;
+  const signature = aesEncrypt(combox_key, secretKey, accessKey);
+  console.log(signature);
+  const header = {
+    'User-Agent': 'python-requests/2.25.1',
+    'Content-Type': 'application/json',
+    'ACCEPT': 'application/json',
+    'Accept-Encoding': 'gzip, deflate',
+    'accessKey': accessKey,
+    'signature': signature
+  };
+  s.headers = { ...s.headers, ...header };
+  return s;
+};
 interface RequestDrawerProps {
   drawerOpen: boolean;
   record: any;
@@ -72,12 +96,19 @@ export default (props: RequestDrawerProps) => {
       }
     </>;
   };
+  /**
+   * 格式化文本
+   * @param value - 需要格式化的字符串
+   * @returns 格式化后的文本
+   */
   const formatText = (value: string) => {
     let text = '';
     try {
-      text = JSON.stringify(JSON.parse(value), null, 4);
+      text = JSON.stringify(JSON.parse(value), null, 4);  // 将字符串解析为JSON对象，再将JSON对象转换为格式化后的字符串
+      console.log(' 解析成功', text);
     } catch (e) {
-      text = value;
+      text = value;  // 如果解析失败，则直接使用原始字符串
+      console.log( '解析失败', e);
     }
     return text;
   };
@@ -131,6 +162,49 @@ export default (props: RequestDrawerProps) => {
       <pre>{formatText(response)}</pre>
     </>;
   };
+  const CustomPayload = (props: { path: string }) => {
+    const { path } = props;
+    const [data, setData] = useState<any>(null);
+    let s = { headers: {} }; // 创建一个空的请求头对象
+    s = setHeaders(s, 'KUPXCvnjCna9ANc9', 'ptylbHok5nZSGexx'); // 设置请求头
+    useEffect(() => {
+      // 异步操作
+      const fetchData = async () => {
+        const requestBody = {
+          name: path,
+          projectId: ''
+        };
+        const apiUrl = `http://10.50.2.3:8081/api/definition/queryCaseInfo1`;
+        try {
+          const response = await fetch(apiUrl, {
+            method: 'POST',
+            body: JSON.stringify(requestBody),
+            headers: Object.assign({}, s.headers),
+          });
+          console.log(' response', response);
+          const jsonData = await response.json();
+          console.log(' jsonData', jsonData);
+          setData(jsonData);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+
+      fetchData();
+    }, [path]);
+
+    if (data) {
+      // 使用 formatText 函数对数据进行格式化
+      const formattedData = formatText(JSON.stringify(data || {}, null, 4));
+      return <>
+        <pre>{formattedData}</pre>
+      </>;
+    }
+
+    return <>
+      <pre>loading...</pre>
+    </>; // 或者可以返回一个 loading 状态的 UI，表示正在加载数据
+  };
 
   const title = record && record.request.url.match('[^/]+(?!.*/)');
   return <Drawer
@@ -170,6 +244,11 @@ export default (props: RequestDrawerProps) => {
           key: '3',
           children: <Wrapper><Response/></Wrapper>,
         },
+        {
+          label: `Custom Payload`,
+          key: '4',
+          children: <Wrapper><CustomPayload path={ record.request.url.pathname }/></Wrapper>,
+        }
       ]}
     />
   </Drawer>;
